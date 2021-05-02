45e01bde4e80f86eed768705b6a5f492a3dfa2fd
iiTONELOC <anthonytropeano@me.com>
2021-05-02 17:28:08 -0400

    comments, cleaning up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,13 +7,16 @@ require('dotenv').config();
 
 router.get('/', async (req, res) => {
 
-    // GRAB USER IP AND FETCH WEATHER FROM OPEN WEATHER
-    // INCLUDED CODE WORKS LOCALLY AND ON HEROKU 
+    // GRABS USER IP AUTOMAGICALLY
     const geoip = require('geoip-lite');
+    // GRAB USER IP OFF OF REQUEST
     let client = req.clientIp
+    // GRABS GEO-DATA FROM IP IF DEPLOYED
     let ip = geoip.lookup(client)
     let lat;
     let lon;
+
+    // IF LOCAL
     if (client === '::1') {
         client = await Location.user()
         lat = client.lat;
@@ -21,26 +24,27 @@ router.get('/', async (req, res) => {
         city = client.city;
         state = client.state
     } else {
+        // IF DEPLOYED
         city = ip.city;
         state = ip.region;
         lat = ip.ll[0];
         lon = ip.ll[1];
     }
-
+    /* 
+    GRABS WEATHER DATA FROM OPEN WEATHER's ONE CALL API
+    Weather.oneCall is a class method that fetches, returns, and packages the data
+    */
     const data = await Weather.oneCall(lat, lon).then(res => {
         return res
     })
-
+    // DESTRUCTURE PACKAGED DATA
     const {
         cw, mw, dw, condition, hourly, hourlyExtended,alerts
     } = data
-
+    // RETURN RESPONSE AND RENDER PAGE
     res.render('homepage', {
         cw, mw, dw, city, state, condition, hourly, hourlyExtended, alerts
     })
-
-
-    // ==============================================
 })
 
 
